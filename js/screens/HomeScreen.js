a327dbd6505cee5da89364320347ef2f11ca5102
julienr2 <julien.rougeron@gmail.com>
2018-10-05 03:46:01 -0700

    add test for marker image,

--- js/screens/HomeScreen.js
+++ js/screens/HomeScreen.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Image } from 'react-native';
+import MapView from 'react-native-maps';
+import FastImage from 'react-native-fast-image';
 
 import Header from '../components/Header';
 import Map from '../components/Map';
+import Text from '../components/Text';
 
 export default class HomeScreen extends React.Component {
   static propTypes = {
@@ -22,7 +25,28 @@ export default class HomeScreen extends React.Component {
             top: 0,
             bottom: 0,
           }}
-        />
+        >
+          { Array(100).fill().map((_, i) => (
+            <MapView.Marker
+              key={`image${i}`}
+              coordinate={{
+                latitude: Math.floor(Math.random() * (2 * 85)) - 85,
+                longitude: Math.floor(Math.random() * (2 * 180)) - 180,
+              }}
+              tracksViewChanges={false}
+            >
+              <Text>YO</Text>
+              <Image
+                style={{
+                  height: 30,
+                  width: 30,
+                }}
+                resizeMethod="scale"
+                source={{ uri: 'https://picsum.photos/100/100/?image=' + i }}
+              />
+            </MapView.Marker>
+          ))}
+        </Map>
       </View>
     );
   }
