e734e4924d95f2a37e10b3d14e2839c05798d921
julienr2 <julien.rougeron@gmail.com>
2018-10-04 20:39:54 -0700

    lint

--- js/screens/HomeScreen.js
+++ js/screens/HomeScreen.js
@@ -17,7 +17,6 @@ export default class HomeScreen extends React.Component {
       <View style={styles.container}>
         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
           <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
 
             <Text style={styles.getStartedText}>Get started by opening</Text>
 
