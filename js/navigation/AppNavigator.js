56e9796c69f202a739dcc71fd27ea8380de79753
julienr2 <julien.rougeron@gmail.com>
2018-10-18 13:19:22 -0700

    add friends screen

--- js/navigation/AppNavigator.js
+++ js/navigation/AppNavigator.js
@@ -4,9 +4,11 @@ import HomeNavigator from './HomeNavigator';
 import AddReviewScreen from '../screens/AddReviewScreen';
 import AddImageScreen from '../screens/AddImageScreen';
 import EditProfileScreen from '../screens/EditProfileScreen';
+import FriendsScreen from '../screens/FriendsScreen';
 
 export default createStackNavigator({
   Home: HomeNavigator,
+  FriendsScreen,
   AddReviewScreen,
   AddImageScreen,
   EditProfileScreen,
--- js/screens/SidebarScreen.js
+++ js/screens/SidebarScreen.js
@@ -49,6 +49,8 @@ https://play.google.com/store/apps/details?id=com.nowmad`,
 
   navigateToProfile = () => this.props.navigation.navigate('EditProfileScreen');
 
+  navigateToFriends = () => this.props.navigation.navigate('FriendsScreen');
+
   onLogoutPress = () => {
     apiLogout(this.props.dispatch)
       .then(() => {
